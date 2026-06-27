49a8fcb22ffd96704bd00342858c58469ec07e76
Arfaz-Shaikh <oceanarfaz@gmail.com>
2026-06-27 19:57:26 +0530

    Delete SQL/Profit Margin by Category.sql

--- SQL/Profit Margin by Category.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    category,
-    ROUND(SUM(sales),2) AS sales,
-    ROUND(SUM(profit),2) AS profit,
-    ROUND((SUM(profit)/SUM(sales))*100,2) AS profit_margin
-FROM superstore
-GROUP BY category
-ORDER BY profit_margin DESC;
